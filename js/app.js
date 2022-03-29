debcf402d11b671450e992f5aa94e722c82452f0
Soe Myat Myat <soemyatmyat@protonmail.com>
2022-03-30 00:52:24 +0800

    PWA Skeleton First commit

---
+++ js/app.js
@@ -0,0 +1,59 @@
+const hIn=document.getElementById('hIn')
+const wIn=document.getElementById('wIn')
+const bmi=document.getElementById('bmi');
+
+//output.style.visibility = 'hidden';
+
+
+hIn.addEventListener('input', (e) => {
+  let hInVal=hIn.value/100
+  let wInVal=wIn.value
+  let hInSqr=hInVal*hInVal
+  if (hInVal != "" && wInVal !="") {
+    bmi.innerHTML=(wInVal/hInSqr).toFixed(2)
+  }
+  e.preventDefault();
+});
+
+wIn.addEventListener('input', (e) => {
+    let hInVal=hIn.value/100
+    let wInVal=wIn.value
+    let hInSqr=hInVal*hInVal
+    if (hInVal != "" && wInVal !="") {
+      bmi.innerHTML=(wInVal/hInSqr).toFixed(2)
+    }
+    e.preventDefault();
+});
+  
+
+// Feature detection - check if browser supports push messaging.
+const check=() => {
+    if (!('serviceWorker' in navigator)) {
+        throw new Error('Service Worker is not supported on this browser.')
+    } else {
+        return ('serviceWorker' in navigator)
+    }
+}
+
+// Register a Service Worker
+const registerServiceWorker=async () => { 
+    //register the serviceworker, supply where serviceworker file is located
+    const swRegistration = await navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        console.log('[Service Worker] Registration succeeded', registration);
+      }, /*catch*/ function(error) {
+        console.log('[Service Worker] registration failed:', error);
+    })
+    return swRegistration
+}
+
+//1. load the service worker js and register it
+const main = async () => { //async loading the service worker
+    check()
+    const swRegistration = await registerServiceWorker()
+}
+
+// consolidate the functions to trigger upon page load
+
+window.addEventListener('load',()=>{
+    main()
+})
---
+++ sw.js
@@ -0,0 +1,33 @@
+const cacheName="BMI-calculator"
+// let's cache the HTML, CSS, JS and any static files that make up the application shell
+const appShellFiles=[
+    '../index.html',
+    './', //alias for index.html
+    '../css/style.css',
+    '../js/app.js',
+    '../img/icons/favicon.ico',
+    '../img/icons/android-chrome-192x192.png',
+    '../img/icons/android-chrome-512x512.png'
+]
+
+//1. Install the service worker 
+self.addEventListener('install',(e)=> {
+    console.log('[Service Worker] Installation Complete')
+    e.waitUntil((async ()=>{
+        const cache=await caches.open(cacheName)
+        console.log('[Service Worker] Caching...')
+        await cache.addAll(appShellFiles)
+        console.log('[Service Worker] Cache Complete')
+    })())
+})
+
+//2. Activate the service worker
+self.addEventListener('activate',(e)=>{
+    console.log('[Service Worker] Activated')
+})
+
+
+//3. trigger based - fetch event fires every time any resources controlled by a sw is fetched
+self.addEventListener('fetch',(e)=>{
+    console.log('[Service Worker] Fetching Info...')
+})
